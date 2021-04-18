8c920843a9bbd9aa93f4e14208cbaadfab5a4f1f
Toni Hutter <hutter.toni@gmail.com>
2021-04-18 16:18:32 +0200

    Add tailwind

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Text } from 'react-native';
+import tailwind from 'tailwind-rn';
+
 import { ROUTES } from '../../config';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'blue',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    fontSize: 30,
-    color: 'white'
-  }
-});
-
 const HomeScreen = ({ navigation }) => (
-  <View style={styles.container}>
+  <View style={tailwind('h-screen bg-blue-300 flex flex-1 items-center')}>
     <TouchableOpacity
       onPress={() => navigation.navigate(ROUTES.HOMESTACK.SECOND)}
     >
-      <Text style={styles.text}>Next Screen</Text>
+      <Text style={tailwind('text-white text-3xl')}>Next Screen</Text>
     </TouchableOpacity>
   </View>
 );
