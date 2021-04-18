dd55dd6d964c0efa0e38f839a8e9545166b74058
louis <louis34@gmx.de>
2021-04-18 19:49:35 +0200

    💄 ✨

--- src/components/CustomDrawer.js
+++ src/components/CustomDrawer.js
@@ -1,34 +1,77 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
 import {
   SafeAreaView,
-  StyleSheet,
   TouchableOpacity,
   View,
   Text,
+  StyleSheet,
 } from 'react-native';
 import tailwind from 'tailwind-rn';
 
-const CustomDrawer = () => {
-  const textStyle = tailwind(
-    'text-2xl font-semibold tracking-wider text-gray-800',
+import { GET_CATEGORIES, ROUTES } from '../config';
+
+const styles = StyleSheet.create({
+  shadow: {
+    elevation: 10,
+  },
+});
+
+const CustomDrawer = ({ navigation }) => {
+  const { data } = useQuery(GET_CATEGORIES);
+
+  const mainTextStyle = tailwind(
+    'text-2xl font-semibold tracking-widest text-gray-800 uppercase',
   );
 
   return (
     <SafeAreaView style={tailwind('flex flex-1 bg-gray-200')}>
-      <View style={tailwind('flex flex-1 flex-col mt-40 ml-8')}>
-        <TouchableOpacity>
-          <Text style={textStyle}>Home</Text>
+      <View
+        style={[
+          tailwind('items-center mt-8 ml-5 mr-5 bg-pink-600 p-2 rounded-xl'),
+          styles.shadow,
+        ]}
+      >
+        <Text
+          style={tailwind(
+            'text-4xl font-bold tracking-widest uppercase text-white',
+          )}
+        >
+          JAMSTACK
+        </Text>
+      </View>
+      <View style={tailwind('flex flex-1 flex-col mt-20 ml-8')}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(ROUTES.NESTEDSTACK.HOME)}
+        >
+          <Text style={mainTextStyle}>Home</Text>
+          <View style={tailwind('h-0.5 bg-pink-600 mt-1 mb-8')} />
         </TouchableOpacity>
-        <View>
-          <TouchableOpacity>
-            <View>
-              <Text></Text>
-            </View>
-          </TouchableOpacity>
+        <View style={tailwind('ml-4')}>
+          {data &&
+            Array.isArray(data.categories) &&
+            data.categories.map((itm, i) => (
+              <TouchableOpacity
+                key={`category-${i}`}
+                onPress={() => navigation.navigate(ROUTES.NESTEDSTACK.HOME)}
+              >
+                <Text
+                  style={tailwind(
+                    'text-xl font-semibold tracking-wider text-gray-800 uppercase',
+                  )}
+                >
+                  {itm?.Name}
+                </Text>
+                <View style={tailwind('h-0.5 bg-pink-600 mt-1 mb-4')} />
+              </TouchableOpacity>
+            ))}
         </View>
-        <View style={tailwind('h-0.5 bg-black my-8')} />
-        <TouchableOpacity>
-          <Text style={textStyle}>Products</Text>
+        <TouchableOpacity
+          style={tailwind('mt-5')}
+          onPress={() => navigation.navigate(ROUTES.NESTEDSTACK.HOME)}
+        >
+          <Text style={mainTextStyle}>Products</Text>
+          <View style={tailwind('h-0.5 bg-pink-600 mt-1 mb-8')} />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
