8f6631981640e60ae2fa0da7d36413f65f2ad8bc
louis <louis34@gmx.de>
2021-04-18 15:48:50 +0200

    lel

--- ios/reactNativeGatsbyAppTests/reactNativeGatsbyAppTests.m
+++ ios/reactNativeGatsbyAppTests/reactNativeGatsbyAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactNativeGatsbyAppTests : XCTestCase
+@interface caraTests : XCTestCase
 
 @end
 
-@implementation reactNativeGatsbyAppTests
+@implementation caraTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
